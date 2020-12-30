58b393c0cfc68dedc79987ed9a69058b651611ed
Thien <thiennguyen@beeknights.com>
2020-12-30 12:02:22 +0700

    first commit

---
+++ selling/selling/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  selling
+//
+//  Created by Thien on 12/30/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("nice")
+    }
+
+
+}
+
